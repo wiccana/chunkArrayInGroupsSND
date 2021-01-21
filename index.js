d3433c0890ca2869a190186c0bbefedb24e0a3e9
brenda.mendez@sondeos.com.ar <brenda.mendez@sondeos.com.ar>
2021-01-21 17:41:39 -0300

    solucion

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const chunkArrayInGroups = function(arr, size){
+       
+    let newArr = []
+     let auxArr = []
 
-    /* YOUR CODE HERE */
+     for(let i = 0; i < arr.length; i++){
+
+        auxArr.push(arr[i])
+     
+        if( auxArr.length == size || i == arr.length -1){
+            console.log('push')
+            newArr.push(auxArr)
+            auxArr = []
+        }
+
+
+    }
+     
+    console.log('newArr', newArr)
+     return newArr
+
+   
 
 }
 
